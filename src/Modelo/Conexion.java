654e37c7afd7628b7d7ca6d34123684f7084d29c
MichaelRamos03 <sr22010@ues.edu.sv>
2024-10-23 14:56:47 -0600

    Subiendo clase conexion

---
+++ src/Modelo/Conexion.java
@@ -0,0 +1,56 @@
+
+package Modelo;
+
+import ds.desktop.notify.DesktopNotify;
+import ds.desktop.notify.NotifyTheme;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Michael Ramos;
+**/
+public class Conexion {
+    
+     Connection cn;
+
+    public Conexion() {
+        
+    }
+    
+    public Connection getConexion(){
+        String user ="postgres";
+        String clave = "root";
+        String url = "jdbc:postgresql://localhost:5432/gestionClinica";
+        
+        try{
+            this.cn = DriverManager.getConnection(url, user, clave);
+            DesktopNotify.setDefaultTheme(NotifyTheme.Green);
+            DesktopNotify.showDesktopMessage("Exito", "la conexion tubo exito",
+            DesktopNotify.ERROR, 3000);
+            
+        }catch(Exception ex){
+            DesktopNotify.setDefaultTheme(NotifyTheme.Red);
+            DesktopNotify.showDesktopMessage("Error", "Error en la conexion a la base",
+            DesktopNotify.ERROR, 3000);
+        }
+        return cn;
+    }
+    
+    public void closeConexion(Connection conn){
+        try{
+            if (conn != null) {
+                conn.close();
+            }
+        }catch(Exception sql){
+            sql.printStackTrace();
+            DesktopNotify.setDefaultTheme(NotifyTheme.Red);
+            DesktopNotify.showDesktopMessage("Error", "Error al cerrar la conexion a la base",
+            DesktopNotify.ERROR, 3000);   
+        }
+    }
+}
